a65ad0919ff5b8069cf2bf3ba3d4ffb6d06a2652
kanis <kanis_pla@hotmail.co.th>
2021-12-12 03:31:46 +0700

    first commit

---
+++ models/Document.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const documentScheme = new mongoose.Schema({
+   value: {
+       type: String,
+       required: true
+
+  }
+}) 
+
+module.exports = mongoose.model("Document",
+documentScheme)
